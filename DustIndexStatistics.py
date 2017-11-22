afcd93c4616ec08183a0c7cd790960b062f11b03
Tobias Machnitzki <tobias.machnitzki@mpimet.mpg.de>
2017-11-22 13:00:42 +0100

    created a class for handling the dustIndex

--- DustIndexStatistics.py
+++ DustIndexStatistics.py
@@ -9,6 +9,58 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 
+class DustIndex:
+    from netCDF4 import Dataset
+    import pandas as pd
+
+    def __init__(self,datestr):
+        self.dataFrame = self.__openNcFile(datestr)
+        self.DI_low,self.DI_high,self.updateTime = self.__getDI()
+
+
+    def __openNcFile(self,datestr):
+        # Parameter:
+        NC_PATH = "/pool/OBS/ACPC/RamanLidar-LICHT/3_QuickLook/nc/ql{}/".format(datestr[2:6])
+        NC_NAME = "/li{}.b532".format(datestr[2:])
+
+        # Get data from nc-file:
+        nc = Dataset(NC_PATH + NC_NAME)
+        dustIndexLow = nc.variables["DustIndexLowLayer"][:].copy()
+        dustIndexTotal = nc.variables["DustIndexTotal"][:].copy()
+        seconds = nc.variables["Time"][:].copy()
+        nc.close()
+
+        # Handle missing Values:
+        dustIndexTotal[np.where(dustIndexTotal > 1e30)] = np.nan
+        dustIndexLow[np.where(dustIndexLow > 1e30)] = np.nan
+
+        dustIndexHigh = np.subtract(dustIndexTotal,dustIndexLow)
+
+        # Shape time to the right format:
+        time = []
+        start_time = dt(int(datestr[:4]), int(datestr[4:6]), int(datestr[6:]), 0, 0, 0)
+        for t in seconds:
+            time.append(start_time + timedelta(seconds=int(t)))
+        time = np.asarray(time)
+
+        df = self.pd.DataFrame({'time': time, 'DIL': dustIndexLow, 'DIH': dustIndexHigh})
+        # df = df[::-1]  # reverse columns
+        return df
+
+    def __getDI(self):
+        __dil = self.dataFrame["DIL"][-30:].mean() # Each two minutes we get a measurement. --> last hour = the last 30 values
+        __dih = self.dataFrame["DIH"][-30:].mean()
+        __up_time = self.dataFrame["time"].tail(1).values[0]
+        __ts = self.pd.to_datetime(str(__up_time))
+        __d = __ts.strftime('%Y %m %d, %H:%M:%S')
+        return(__dil,__dih,__d)
+
+
+
+
+
+
+
 if __name__ == "__main__":
 
     start = dt(2017,5,1)
@@ -95,5 +147,3 @@ if __name__ == "__main__":
     ax6.boxplot(total_values,1,"+")
     ax5.boxplot(low_values,5,"+")
     plt.savefig("DI_stats.png")
-
-
